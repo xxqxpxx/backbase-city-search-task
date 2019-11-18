c610abf793d16614890f260a441ad73e84e966c2
xxqxpxx <ahmedafatah@outlook.com>
2019-11-18 15:48:33 +0000

    add code in files types: .java,.xml remove code in files types: .java,.xml

--- content.java
+++ content.java
@@ -19,6 +19,6 @@ public class Crggga {
         System.out.println("mtsla");
         System.out.println("mkbxj");
         System.out.println("jicky");
-        System.out.println("tnsci");
+        System.out.println("vjmsh");
     }
 }

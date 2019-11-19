ffa1235a9c0339ecfaa58d516c172f3d9fedbae1
Ahmed Abdul Fatah <ahmedafatah@outlook.com>
2019-11-19 11:45:09 +0000

    add code in files types: .java,.xml remove code in files types: .java

--- content.java
+++ content.java
@@ -89,6 +89,6 @@ public class Crggga {
         System.out.println("qcngn");
         System.out.println("cxskq");
         System.out.println("bbjoy");
-        System.out.println("woiej");
+        System.out.println("wrktj");
     }
 }

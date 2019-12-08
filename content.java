cb190e8b3065dab8964c2790bf3527cd69a45b04
xxqxpxx <ahmedafatah@outlook.com>
2019-12-08 10:56:21 +0000

    add code in files types: .gradle,.java remove code in files types: .gradle,.kt,.java,.xml

--- content.java
+++ content.java
@@ -54,6 +54,6 @@ public class Crggga {
         System.out.println("lkhmo");
         System.out.println("hquqg");
         System.out.println("ewnuc");
-        System.out.println("oclrg");
+        System.out.println("hdbkb");
     }
 }

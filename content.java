adba806149855944b4ae976fabfb0af759a68353
xxqxpxx <ahmedafatah@outlook.com>
2019-12-25 11:38:28 +0000

    add code in files types: .gradle,.json,.kt,.xml,.java,.kt},.xml},.log remove code in files types: .gradle,.xml,.kt,.java,.kt},.xml}

--- content.java
+++ content.java
@@ -53,6 +53,6 @@ public class Crggga {
         System.out.println("dvjbh");
         System.out.println("lkhmo");
         System.out.println("hquqg");
-        System.out.println("jvoph");
+        System.out.println("wdipw");
     }
 }

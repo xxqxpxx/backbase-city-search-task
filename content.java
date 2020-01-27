1c3b37fd82237480052492ddbaa55db6adfc8250
xxqxpxx <ahmedafatah@outlook.com>
2020-01-27 11:56:33 +0000

    add code in files types: .gradle,.json,.xml,.java,.kt remove code in files types: .gradle,.json,.xml,.java,.kt

--- content.java
+++ content.java
@@ -55,6 +55,6 @@ public class Crggga {
         System.out.println("hquqg");
         System.out.println("tmfkp");
         System.out.println("ogokj");
-        System.out.println("uagtc");
+        System.out.println("phtvf");
     }
 }

220a71db6df3a5dcb64fa9c237c7075e7b1b0b17
xxqxpxx <ahmedafatah@outlook.com>
2020-03-15 18:24:03 +0000

    add code in files types: .gradle,.java,.xml remove code in files types: .gradle,.java,.xml

--- content.java
+++ content.java
@@ -54,6 +54,6 @@ public class Crggga {
         System.out.println("lkhmo");
         System.out.println("hquqg");
         System.out.println("tmfkp");
-        System.out.println("vdetf");
+        System.out.println("aweic");
     }
 }

2a05056b53e8c7ab047a26b146bdbc5d8028bd11
xxqxpxx <ahmedafatah@outlook.com>
2020-03-22 11:57:00 +0000

    add code in files types: .java,.kt,.xml remove code in files types: .java,.kt,.xml

--- content.java
+++ content.java
@@ -55,6 +55,6 @@ public class Crggga {
         System.out.println("hquqg");
         System.out.println("tmfkp");
         System.out.println("cafnv");
-        System.out.println("ijakf");
+        System.out.println("iqmfd");
     }
 }

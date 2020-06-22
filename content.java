457bdacd40b181c4d368f17ca09549dba71367a3
xxqxpxx <ahmedafatah@outlook.com>
2020-06-22 04:25:25 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -22,6 +22,6 @@ public class Crggga {
         System.out.println("lhwcj");
         System.out.println("etyrs");
         System.out.println("vagjt");
-        System.out.println("xqmpr");
+        System.out.println("tkehq");
     }
 }

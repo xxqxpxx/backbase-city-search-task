95b1fd56edb35b61cda263663d3182d0a80b1b73
xxqxpxx <ahmedafatah@outlook.com>
2021-05-31 15:51:58 +0000

    add code in files types: .pro,.xml,.json,.java,.kt, remove code in files types: .xml,.json,.java,.kt,

--- content.java
+++ content.java
@@ -10,6 +10,6 @@ public class Crggga {
         System.out.println("mobjn");
         System.out.println("fwuae");
         System.out.println("kddgj");
-        System.out.println("mjfwp");
+        System.out.println("xlbvj");
     }
 }

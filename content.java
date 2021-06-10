01bb2319427e9f1ab183721000f1f831669b6830
xxqxpxx <ahmedafatah@outlook.com>
2021-06-10 15:29:33 +0000

    add code in files types: .java,.kt remove code in files types: .java,.kt

--- content.java
+++ content.java
@@ -10,6 +10,6 @@ public class Crggga {
         System.out.println("mobjn");
         System.out.println("fwuae");
         System.out.println("kddgj");
-        System.out.println("qxxnf");
+        System.out.println("oejdy");
     }
 }

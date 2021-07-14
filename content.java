f164fa6b8812ff015ef794642a8c07eb0fb9d028
xxqxpxx <ahmedafatah@outlook.com>
2021-07-14 12:32:10 +0000

    add code in files types: .java remove code in files types:

--- content.java
+++ content.java
@@ -46,5 +46,6 @@ public class Crggga {
         System.out.println("lxslw");
         System.out.println("lcnsn");
         System.out.println("yjjkw");
+        System.out.println("iplkb");
     }
 }

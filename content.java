011bee6ef0677890e017b2dab8523f5a2841f685
Ahmed Abdul Fatah <ahmedafatah@outlook.com>
2023-05-17 17:49:21 +0000

    add code in files types: .gradle,.java,.xml, remove code in files types:

--- content.java
+++ content.java
@@ -252,5 +252,6 @@ public class Crggga {
         System.out.println("khoro");
         System.out.println("gjxxj");
         System.out.println("fqvnn");
+        System.out.println("tyfxw");
     }
 }
